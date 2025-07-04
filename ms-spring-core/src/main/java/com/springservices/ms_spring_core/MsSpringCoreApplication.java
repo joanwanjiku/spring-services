87b15a9e58a69ba345916ef2c79957b93c96d5ad
joanwanjiku <joanevans18@gmail.com>
2025-07-03 21:32:21 -0400

    Component scan

--- ms-spring-core/src/main/java/com/springservices/ms_spring_core/MsSpringCoreApplication.java
+++ ms-spring-core/src/main/java/com/springservices/ms_spring_core/MsSpringCoreApplication.java
@@ -3,11 +3,14 @@ package com.springservices.ms_spring_core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*@SpringBootApplication(
+		scanBasePackages = {"com.springservices.ms_spring_core",
+		"com.springservices.util"}
+)*/
 @SpringBootApplication
 public class MsSpringCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsSpringCoreApplication.class, args);
 	}
-
 }
